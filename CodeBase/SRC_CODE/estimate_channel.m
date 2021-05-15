7b350ead16a6e32b4102d483e4611764e6344a17
zhangyfreddy <freddy.z1999@gmail.com>
2021-05-15 17:55:51 -0500

    finished est channel

--- CodeBase/SRC_CODE/estimate_channel.m
+++ CodeBase/SRC_CODE/estimate_channel.m
@@ -3,5 +3,25 @@ function [ H ] = estimate_channel(rx_sym1,rx_sym2)
 load ../Mat_Files/Parameters.mat 
 
 %  ... add your code here ... %
-
+H = zeros(1,num_bins);
+preamble = zeros(1,num_bins);
+for m=1:1:size(bits_preamble,2)
+    index = 0;
+    if m > 26
+        index = m+12;
+    else
+        index = m+1;
+    end
+    if bits_preamble(m) == 1
+        preamble(index)= -1;
+    else
+        preamble(index) = 1;
+    end
+end
+for m=1:1:num_bins
+    fftbin = convert_bin_index_fft_to_normal(m, num_bins);
+    if sum(gaurd_bins == fftbin) == 0
+        H(m) = abs((rx_sym1(m)+rx_sym2(m))/(2*preamble(m)));
+    end
+end
 end
