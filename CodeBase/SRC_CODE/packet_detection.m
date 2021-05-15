5ece30e163988680d1bcf0311e2cd688b878320e
zhangyfreddy <freddy.z1999@gmail.com>
2021-05-15 14:21:11 -0500

    finished packet detection?

--- CodeBase/SRC_CODE/packet_detection.m
+++ CodeBase/SRC_CODE/packet_detection.m
@@ -9,5 +9,19 @@ rx_signal = filter([1 -1], [1 -alpha], rx_signal);
 threshold=6;
 
 %  ... add your code here ... %
-
+length = size(rx_signal,2);
+for m=1:1:length-103
+    windowA = rx_signal(1,m:m+51);
+    windowB = rx_signal(1,m+52:m+103);
+    powerA = 0;
+    powerB = 0;
+    for n=1:1:52
+       powerA = powerA + windowA(1,n)^2;
+       powerB = powerB + windowB(1,n)^2;
+    end
+    if powerB/powerA > threshold
+        packet_start_index = m;
+        break;
+    end
+end
 end
